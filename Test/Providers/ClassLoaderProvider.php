eb6904bf975ea9bd92ff35574529261dcc514575
mapserver2007 <mapserver2007@gmail.com>
2017-03-26 00:06:46 +0900

    名前空間取得処理を追加

--- Test/Providers/ClassLoaderProvider.php
+++ Test/Providers/ClassLoaderProvider.php
@@ -61,6 +61,14 @@ trait ClassLoaderProvider
         ];
     }
 
+    public function loadNamespaceProvider()
+    {
+        return [
+            [dirname(__FILE__) . '/../Fixtures', 'ImportFixture6.php', ['\WebStream\ClassLoader\Test\Fixtures']],
+            [dirname(__FILE__) . '/../Fixtures', 'ImportFixture7.php', ['\WebStream\ClassLoader\Test\Fixtures\NamespaceSub\Sub1', '\WebStream\ClassLoader\Test\Fixtures\NamespaceSub\Sub2']]
+        ];
+    }
+
     public function unLoadProvider()
     {
         return [
@@ -73,6 +81,13 @@ trait ClassLoaderProvider
     {
         return [
             [dirname(__FILE__) . '/../Fixtures']
+        ];
+    }
+
+    public function unLoadNamespaceProvider()
+    {
+        return [
+            [dirname(__FILE__) . '/../Fixtures', 'Dummy.php', []]
         ];
     }
 }
